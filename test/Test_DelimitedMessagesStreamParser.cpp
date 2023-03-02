36658f9cd024e0218775c4b5fabe71657c13247f
van <valik.silchenko@gmail.com>
2023-03-02 16:56:35 +0300

    test: added test for DelimitedMessagesStreamParser

--- test/Test_DelimitedMessagesStreamParser.cpp
+++ test/Test_DelimitedMessagesStreamParser.cpp
@@ -1,10 +1,34 @@
-/*
- * Test_DelimitedMessagesStreamParser.cpp
- *
- *  Created on: 2 Feb 2023
- *      Author: sia
- */
-#include <protobuf_parser/DelimitedMessagesStreamParser.h>
+#include "protobuf_parser/DelimitedMessagesStreamParser.h"
+#include "util/pr.pb.h"
 
+void test()
+{
+    TestTask::Messages::WrapperMessage message;
+    TestTask::Messages::RequestForSlowResponse* request;
 
+    std::string data;
+    char msg[60];
+    std::shared_ptr<const Data> pointerToMsg;
+    for (int i = 0; i < 5; i++)
+    {
+        request = new TestTask::Messages::RequestForSlowResponse;
+        request->set_time_in_seconds_to_sleep(3 * i);
+        message.set_allocated_request_for_slow_response(request);
+        pointerToMsg = serializeDelimited<TestTask::Messages::WrapperMessage>(message);
+        for (int j = 0; j < pointerToMsg->size(); j++)
+        {
+            data.push_back((*pointerToMsg)[j]);
+        }
+    }
+
+    std::list<DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage>::PointerToConstValue> list;
+
+    DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> parser;
+    list = parser.parse(data);
+
+    for (const auto& pointer : list)
+    {
+        std::cout << (*pointer).DebugString() << "\n";
+    }
+}
 
--- test/Test_Helpers.cpp
+++ test/Test_Helpers.cpp
@@ -1,10 +1,6 @@
-/*
- * Test_Helpers.cpp
- *
- *  Created on: 2 Feb 2023
- *      Author: sia
- */
 #include <protobuf_parser/helpers.hpp>
+
+
 
 
 
