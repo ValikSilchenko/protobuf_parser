8fee441b9f49c25a65d9edd45b55a54823be6e25
van <valik.silchenko@gmail.com>
2023-03-02 17:00:56 +0300

    test: added test for serializeDelimited

--- test/Test_Helpers.cpp
+++ test/Test_Helpers.cpp
@@ -1,7 +1,7 @@
 #include "protobuf_parser/helpers.hpp"
 #include "util/pr.pb.h"
 
-void testParse()
+void testParsing()
 {
     TestTask::Messages::WrapperMessage message;
     auto request = new TestTask::Messages::RequestForSlowResponse;
@@ -25,6 +25,29 @@ void testParse()
     std::cout << response.DebugString() << "\n\n";
 }
 
+void testSerializing()
+{
+    TestTask::Messages::WrapperMessage message;
+    auto request = new TestTask::Messages::RequestForSlowResponse;
+    request->set_time_in_seconds_to_sleep(12);
+    message.set_allocated_request_for_slow_response(request);
 
+    std::cout << message.DebugString() << "\n\n";
+
+    std::shared_ptr<const Data> data = serializeDelimited<TestTask::Messages::WrapperMessage>(message);
+    char* msg = new char[data->size()];
+    for (int i = 0; i < data->size(); i++)
+    {
+        msg[i] = (*data)[i];
+    }
+    std::shared_ptr<TestTask::Messages::WrapperMessage> res = parseDelimited<TestTask::Messages::WrapperMessage>(msg, data->size());
+
+    if (res)
+    {
+        TestTask::Messages::WrapperMessage response = *(res);
+        std::cout << response.DebugString() << "\n\n";
+    }
+    delete [] msg;
+}
 
 
