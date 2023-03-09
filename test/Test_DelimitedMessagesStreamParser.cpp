c828f90552ae81306cd8206efd0c27a99ce65b17
van <valik.silchenko@gmail.com>
2023-03-09 10:50:53 +0300

    tests: added tests for DelimitedMessagesStreamParser

--- test/Test_DelimitedMessagesStreamParser.cpp
+++ test/Test_DelimitedMessagesStreamParser.cpp
@@ -1,19 +1,26 @@
+#include <gtest/gtest.h>
 #include "protobuf_parser/DelimitedMessagesStreamParser.h"
-#include "util/pr.pb.h"
+#include "proto/pr.pb.h"
 
-void test()
+
+TEST(ParserTests, test_parsing_with_full_msg)
 {
+    int testNum = 3;
+
     TestTask::Messages::WrapperMessage message;
     TestTask::Messages::RequestForSlowResponse* request;
 
     std::string data;
-    char msg[60];
     std::shared_ptr<const Data> pointerToMsg;
+    std::list<DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage>::PointerToConstValue> list;
+    DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> parser;
+
     for (int i = 0; i < 5; i++)
     {
         request = new TestTask::Messages::RequestForSlowResponse;
-        request->set_time_in_seconds_to_sleep(3 * i);
+        request->set_time_in_seconds_to_sleep(testNum * i);
         message.set_allocated_request_for_slow_response(request);
+
         pointerToMsg = serializeDelimited<TestTask::Messages::WrapperMessage>(message);
         for (int j = 0; j < pointerToMsg->size(); j++)
         {
@@ -21,14 +28,51 @@ void test()
         }
     }
 
-    std::list<DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage>::PointerToConstValue> list;
+    list = parser.parse(data);
+    int numToCompare = 0;
+    for (const auto& msg : list)
+    {
+        ASSERT_TRUE(msg->has_request_for_slow_response());
+        ASSERT_EQ(msg->request_for_slow_response().time_in_seconds_to_sleep(), numToCompare);
+        numToCompare += testNum;
+    }
+}
 
+TEST(ParserTests, test_parsing_with_part_of_msg)
+{
+    int testNum = 3;
+
+    TestTask::Messages::WrapperMessage message;
+    TestTask::Messages::RequestForSlowResponse* request;
+
+    std::string data;
+    std::shared_ptr<const Data> pointerToMsg;
+    std::list<DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage>::PointerToConstValue> list;
     DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> parser;
+
+    request = new TestTask::Messages::RequestForSlowResponse;
+    request->set_time_in_seconds_to_sleep(testNum);
+    message.set_allocated_request_for_slow_response(request);
+
+    pointerToMsg = serializeDelimited<TestTask::Messages::WrapperMessage>(message);
+    for (int j = 0; j < pointerToMsg->size() / 2; j++)
+    {
+        data.push_back((*pointerToMsg)[j]);
+    }
+
+    list = parser.parse(data);
+    ASSERT_TRUE(list.empty());
+
+    data.clear();
+    for (int j = pointerToMsg->size() / 2; j < pointerToMsg->size(); j++)
+    {
+        data.push_back((*pointerToMsg)[j]);
+    }
+
     list = parser.parse(data);
 
-    for (const auto& pointer : list)
-    {
-        std::cout << (*pointer).DebugString() << "\n";
-    }
+    ASSERT_TRUE(list.front()->has_request_for_slow_response());
+    ASSERT_EQ(list.front()->request_for_slow_response().time_in_seconds_to_sleep(), testNum);
+
 }
 
