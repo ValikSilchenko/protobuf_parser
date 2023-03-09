76315ff616185746ae5666c4667ae8aa5ad0a198
van <valik.silchenko@gmail.com>
2023-03-09 10:51:38 +0300

    ci: configured CMakeLists for tests, changed required cmake version

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,10 @@
-#include "proto/pr.pb.h"
-#include "protobuf_parser/DelimitedMessagesStreamParser.h"
-#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-int main() {
-    TestTask::Messages::WrapperMessage message;
-    TestTask::Messages::RequestForSlowResponse* request;
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
 
-    std::string data;
-    std::shared_ptr<const Data> pointerToMsg;
-    for (int i = 0; i < 5; i++)
-    {
-        request = new TestTask::Messages::RequestForSlowResponse;
-        request->set_time_in_seconds_to_sleep(3 * i);
-        message.set_allocated_request_for_slow_response(request);
-        pointerToMsg = serializeDelimited<TestTask::Messages::WrapperMessage>(message);
-        for (int j = 0; j < pointerToMsg->size(); j++)
-        {
-            data.push_back((*pointerToMsg)[j]);
-        }
-    }
-
-    std::list<DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage>::PointerToConstValue> list;
-
-    DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> parser;
-    list = parser.parse(data);
-
-    for (const auto& pointer : list)
-    {
-        std::cout << (*pointer).DebugString() << "\n";
-    }
-
-    return 0;
+    return RUN_ALL_TESTS();
 }
-
