a7c07ca7f922ed6922520d7f0aa39481b8334396
vsilchenko <valik.silchenko@gmail.com>
2023-03-16 10:48:37 +0300

    test: added test for wrong message format

--- test/Test_Helpers.cpp
+++ test/Test_Helpers.cpp
@@ -23,7 +23,7 @@ TEST(HelpersTests, test_serialize_and_parsing_full_msg)
 
     size_t bytesConsumed;
     res = parseDelimited<TestTask::Messages::WrapperMessage>(static_cast<const void*>(&*data), data->size(), &bytesConsumed);
-    ASSERT_TRUE(res.get() != nullptr);
+    ASSERT_TRUE(res != nullptr);
     ASSERT_EQ(bytesConsumed, msgSize + variantByteSize);
     response = *res;
 
@@ -51,11 +51,45 @@ TEST(HelpersTests, test_serialize_and_parsing_msg_by_parts)
 
     size_t bytesConsumed;
     res = parseDelimited<TestTask::Messages::WrapperMessage>(static_cast<const void*>(&*data), data->size() / 2, &bytesConsumed);
-    ASSERT_TRUE(res.get() == nullptr);
+    ASSERT_TRUE(res == nullptr);
     ASSERT_EQ(bytesConsumed, 0);
 
     res = parseDelimited<TestTask::Messages::WrapperMessage>(static_cast<const void*>(&*data), data->size(), &bytesConsumed);
-    ASSERT_TRUE(res.get() != nullptr);
+    ASSERT_TRUE(res != nullptr);
+    ASSERT_EQ(bytesConsumed, msgSize + variantByteSize);
+    response = *res;
+
+    ASSERT_TRUE(response.has_request_for_slow_response());
+    ASSERT_EQ(response.request_for_slow_response().time_in_seconds_to_sleep(), testNum);
+}
+
+TEST(HelpersTests, test_parsing_wrong_format_msg)
+{
+    int testNum = 12;
+    std::string serializedMessage;
+
+    TestTask::Messages::WrapperMessage message;
+    TestTask::Messages::WrapperMessage response;
+    std::shared_ptr<TestTask::Messages::WrapperMessage> res;
+    std::shared_ptr<const Data> data;
+    auto request = new TestTask::Messages::RequestForSlowResponse;
+
+    request->set_time_in_seconds_to_sleep(testNum);
+    message.set_allocated_request_for_slow_response(request);
+
+    uint32_t msgSize = PROTOBUF_MESSAGE_BYTE_SIZE(message);
+    size_t variantByteSize = google::protobuf::io::CodedOutputStream::VarintSize32(msgSize);
+    data = serializeDelimited(message);
+
+    size_t bytesConsumed;
+    std::shared_ptr<Data> wrongData = std::make_shared<Data>(Data(data->cbegin(), --data->cend()));
+    wrongData->push_back('\b');
+    res = parseDelimited<TestTask::Messages::WrapperMessage>(static_cast<const void*>(&*wrongData), wrongData->size() / 2, &bytesConsumed);
+    ASSERT_TRUE(res == nullptr);
+    ASSERT_EQ(bytesConsumed, 0);
+
+    res = parseDelimited<TestTask::Messages::WrapperMessage>(static_cast<const void*>(&*data), data->size(), &bytesConsumed);
+    ASSERT_TRUE(res != nullptr);
     ASSERT_EQ(bytesConsumed, msgSize + variantByteSize);
     response = *res;
 
